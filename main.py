261b7178d0e1e8f6452b99de6ec31b218fcd1962
hackerhgl <hackerhgl@gmail.com>
2023-04-18 08:43:03 +0500

    Wierd behaviour in windows

--- main.py
+++ main.py
@@ -14,14 +14,18 @@ def s_pyttsx3():
     # print(engine.getProperty('voices'))
     voices = engine.getProperty('voices')
     neuter = 'VoiceGenderNeuter'
-    filtered = [voice for voice in voices if voice.languages[0].find("en") > -1 and voice.gender != neuter]
+    print(voices[0])
+    # filtered = [voice for voice in voices if voice.languages[0].find("en") > -1 and voice.gender != neuter]
     # filtered = [voice for voice in voices if voice.languages[0] == 'en_US' and voice.gender != neuter]
     # print(filtered[0].gender)
-    for voice in filtered:
-        print(voice)
-        engine.setProperty('voice', voice.id)  # changes the voice
-        engine.say('The quick brown fox jumped over the lazy dog.')
-        engine.runAndWait()
+    # for voice in filtered:
+    #     print(voice)
+    #     engine.setProperty('voice', voice.id)  # changes the voice
+    #     engine.say('The quick brown fox jumped over the lazy dog.')
+    #     engine.runAndWait()
+
+    engine.say('The quick brown fox jumped over the lazy dog.')
+    engine.runAndWait()
 
     # engine.say("Men of Reddit, what’s the best response to \"Haha you have a small penis?\"")
     # engine.setProperty('rate', 175)
@@ -32,7 +36,7 @@ def s_pyttsx3():
 
 def s_gtts():
     blabla = "Men of Reddit, what’s the best response to \"Hey champ, how's it going? anything good?\""
-    tts = gTTS(text=blabla, lang='en-us')
+    tts = gTTS(text=blabla, lang='en', tld="co.uk")
     safe_mkdir()
     tts.save(os.path.join(TMP_DIR, "test-1.mp3"))
 def main():
